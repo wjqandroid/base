3bf5eca9a13ea307ac3da1f95f5c3c5f94d553a7
weijiaqi <wei.jiaqi@aliyun.com>
2022-04-26 15:55:50 +0800

    代码提交

---
+++ base_module/src/main/java/com/visionvera/library/TestActivity.java
@@ -0,0 +1,16 @@
+package com.visionvera.library;
+
+import com.visionvera.library.base.BaseActivity;
+
+public class TestActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    protected void doYourself() {
+
+    }
+}
